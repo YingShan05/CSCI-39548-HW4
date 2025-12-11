36c18958c687ba08f60e51ff565a4dd045ba6969
Ying Shan Li <yingshanli1118@gmail.com>
2025-12-11 15:21:30 -0500

    Changed frontend api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:5001';
+export const API_BASE = process.env.REACT_APP_API_BASE || 'https://csci-39548-hw4.onrender.com';
 
 async function handleRes(res) {
   if (!res.ok) {
